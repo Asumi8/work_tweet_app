bd556f377217f7845a2b196a2fedb2683aa0dec8
asumi nakajima <nakajima0848@gmail.com>
2022-10-26 16:45:36 +0900

    user:ビューの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path #あとで修正
+      redirect_to user_path(@user.id) #あとで修正
     else
       render :new
     end
