648b34b2f62d3c0a4e555ebd44951c70e55ab129
asumi nakajima <nakajima0848@gmail.com>
2022-10-26 16:25:29 +0900

    userのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :blogs, dependent: :destroy
-  # validates :name
-  # validates :email
-  # validates :password
+  validates :name, presence: true, length: {maximum:30}
+  before_validation {email.downcase!}
+  validates :email, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
+  validates :password, presence: true, length: {minimum:6}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_26_064513) do
+ActiveRecord::Schema.define(version: 2022_10_26_071032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_10_26_064513) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "blogs", "users"
